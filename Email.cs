0f256f11b6bee26cb5b29e9cedb8cec4dc4a8ab3
Brendon W. <bwherry@midland.k12.ia.us>
2026-01-09 12:09:25 -0600

    Fix formatting and casing in Email class

--- Email.cs
+++ Email.cs
@@ -1,11 +1,13 @@
 namespace knightmoves;
+
 public class Email
 {
-   public string introduction = 
+   public string Introduction = 
    "Dear Jake, \n" +
    "\tThank you for being a \"Wonderful\" Co-Worker.\n" +
    "I hpe to see you \'Tomorrow\'.\n\n" +
    "Sincerly,\n" +
    "Dave Miller: \\";
 }
+
 
