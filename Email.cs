b6483b6bef57c0b918f4512e822e2e3c4fe91492
Brendon W. <bwherry@midland.k12.ia.us>
2026-01-09 12:07:15 -0600

    Update Email.cs

--- Email.cs
+++ Email.cs
@@ -1,5 +1,15 @@
 ﻿namespace knightmoves;
 public class Email
 {
-    // Add your code here
+   namespace knightmoves;
+public class Email
+{
+   public string introduction = 
+   "Dear Jake, \n" +
+   "\tThank you for being a \"Wonderful\" Co-Worker.\n" +
+   "I hpe to see you \'Tomorrow\'.\n\n" +
+   "Sincerly,\n" +
+   "Dave Miller: \\";
 }
+}
+
