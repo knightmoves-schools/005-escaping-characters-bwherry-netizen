1d2c23aa9bc7dd690767662428c420a13c664a9f
Brendon W. <bwherry@midland.k12.ia.us>
2026-01-14 11:05:38 -0600

    Update Email.cs

--- Email.cs
+++ Email.cs
@@ -5,9 +5,10 @@ public class Email
    public string Introduction = 
    "Dear Jake, \n" +
    "\tThank you for being a \"Wonderful\" Co-Worker.\n" +
-   "I hpe to see you \'Tomorrow\'.\n\n" +
+   "I hope to see you \'Tomorrow\'.\n\n" +
    "Sincerly,\n" +
    "Dave Miller: \\";
 }
+
 
 
